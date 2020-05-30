6da570c921e071948fac3f614bb9d3e8dc48f23f
Brandon Clapp <brandonclapp@outlook.com>
2020-05-30 12:55:17 -0500

    Add settings file

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -2,12 +2,16 @@ import { Client } from "discord.js";
 import * as fs from "fs";
 
 export async function registerCommands(client: Client) {
+  const settings = require("../../settings.json");
+  console.log("settings");
   fs.readdirSync(__dirname).forEach(async (file) => {
     if (file === "index.ts") {
       return;
     }
 
     const cmd = require(`./${file}`).default;
-    await cmd(client);
+    const section = settings[file.replace(".ts", "")];
+    console.log("section", section);
+    await cmd(client, section);
   });
 }
