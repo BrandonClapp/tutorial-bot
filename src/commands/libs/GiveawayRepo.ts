62c4d6300ae5ddd97bed561fad7db4aedd294756
Brandon Clapp <brandonclapp@outlook.com>
2020-05-30 23:48:14 -0500

    End of ep6, functions for persisting giveaways

--- src/commands/libs/GiveawayRepo.ts
+++ src/commands/libs/GiveawayRepo.ts
@@ -1,3 +1,5 @@
+import * as fs from "fs";
+
 export interface Giveaway {
   id: string;
   expiration: number;
@@ -7,9 +9,37 @@ export interface Giveaway {
 export class GiveawayRepo {
   private dataDir = __dirname + "/../../../data";
 
-  public async save(giveaway: Giveaway): Promise<void> {}
+  public async save(giveaway: Giveaway): Promise<void> {
+    return new Promise((resolve, reject) => {
+      const fileName = `${this.dataDir}/giveaway-${giveaway.id}.json`;
+      fs.writeFile(fileName, JSON.stringify(giveaway), (err) => {
+        if (err) {
+          console.log("Could not write the file", fileName);
+          reject(err);
+          return;
+        }
+
+        resolve();
+      });
+    });
+  }
 
   public async get(id: string): Promise<Giveaway> {
-    return null;
+    return new Promise((resolve, reject) => {
+      const fileName = `${this.dataDir}/giveaway-${id}.json`;
+
+      if (fs.existsSync(fileName)) {
+        fs.readFile(fileName, (err, content) => {
+          if (err) {
+            console.log("Could not read the file", fileName);
+            reject(err);
+          }
+          const giveaway = JSON.parse(content.toString()) as Giveaway;
+          resolve(giveaway);
+        });
+      }
+
+      resolve(null);
+    });
   }
 }
